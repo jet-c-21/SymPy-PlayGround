11fe721178a93abc483a49cf6d5979278e748336
Jet (Ta-Wei Chien) <67193495+jet-c-21@users.noreply.github.com>
2022-03-05 16:21:55 +0800

    fin lect 7

--- WALL/wall_0.py
+++ WALL/wall_0.py
@@ -7,7 +7,14 @@ Create Date: 2022/3/4
 import sympy as sp
 from pprint import pp
 
+sp.init_printing(use_unicode=True)
+
 x = sp.Symbol('x')
+expr = x ** 2
+print(expr)
 eq2 = sp.Eq(sp.cos(x) - sp.sin(x), 0)
-print(type(eq2))
-pp(dir(eq2))
+print(eq2)
+# print(type(eq2))
+# pp(dir(eq2))
+
+sp.Limit
